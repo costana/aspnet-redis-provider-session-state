d12eb63015f72b94e54cee8632456da323bd99ea
Thiago Costa <thiago.costa@jet.com.br>
2022-06-06 19:32:39 -0300

    Add project files.

---
+++ Controllers/RedisSessionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRedisSessionManagement.Controllers
+{
+    public class RedisSessionController : Controller
+    {
+        // GET: RedisSession
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string username, string userid)
+        {
+            Session["usuario"] = username;
+            Session["id"] = userid; 
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
